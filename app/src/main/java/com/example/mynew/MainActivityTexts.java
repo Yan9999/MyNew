56d54cf998f829466d152822584254ea35080bab
Yan NOW <tumakaev.jan@yandex.ru>
2022-06-15 00:22:23 +0500

    Дз 6- ое сделано !!!! Ура

---
+++ app/src/main/java/com/example/mynew/MainActivityTexts.java
@@ -0,0 +1,20 @@
+package com.example.mynew;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityTexts extends AppCompatActivity {
+    public static final String EXTRA_WORKOUT_ID = "id";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_texts);
+        FragmentTexts frag = (FragmentTexts)
+                getSupportFragmentManager().findFragmentById(R.id.detail_frag);
+        int workoutId = (int) getIntent().getExtras().get(EXTRA_WORKOUT_ID);
+        frag.setWorkout(workoutId);
+    }
+}
